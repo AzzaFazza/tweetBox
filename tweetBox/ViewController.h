66ddf56b0f877e57a800137e62318157dc487776
AzzaFazza <adamf@spain.local>
2014-06-30 15:21:00 +0100

    Initial Commit

---
+++ tweetBox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tweetBox
+//
+//  Created by Adam Fallon on 30/06/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tweetBoxTests/tweetBoxTests.m
@@ -0,0 +1,34 @@
+//
+//  tweetBoxTests.m
+//  tweetBoxTests
+//
+//  Created by Adam Fallon on 30/06/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tweetBoxTests : XCTestCase
+
+@end
+
+@implementation tweetBoxTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
